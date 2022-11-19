183a9a6497b300b4400ada6bb15d0da57526cc5e
YgorB <mclygor62@hotmail.com>
2022-11-19 02:06:14 -0300

    teste git

---
+++ script.js
@@ -0,0 +1,10 @@
+let result = document.getElementById('result');
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+let reset = document.getElementById('reset')
+
+function Time(){
+    
+}
+
+start.addEventListener('click', Time)
